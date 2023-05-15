3e822d1fbb66521fd56444dc5e44c85d411af3e3
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-05-15 17:03:08 -0300

    Exibindo o modal

--- compra-de-pizzas/script.js
+++ compra-de-pizzas/script.js
@@ -9,6 +9,15 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        selectElement('.pizzaWindowArea').style.opacity = 0;
+        selectElement('.pizzaWindowArea').style.display = "flex";
+        setTimeout(() => {
+            selectElement('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+    });
 
     selectElement('.pizza-area').append(pizzaItem);
 });
