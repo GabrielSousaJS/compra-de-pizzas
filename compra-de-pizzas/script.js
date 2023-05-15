64e06f5f902b93be8b2996945733cdec59a20dc1
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-05-15 17:16:31 -0300

    Exibindo as informações da pizza no modal

--- compra-de-pizzas/script.js
+++ compra-de-pizzas/script.js
@@ -5,6 +5,7 @@ const selectElements = (e) => document.querySelectorAll(e);
 pizzaJson.map((item, index) => {
     let pizzaItem = selectElement('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
@@ -12,6 +13,12 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
         
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+        selectElement('.pizzaBig img').src = pizzaJson[key].img;
+        selectElement('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        selectElement('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+
         selectElement('.pizzaWindowArea').style.opacity = 0;
         selectElement('.pizzaWindowArea').style.display = "flex";
         setTimeout(() => {
