f26fc1fbdaa63001d1c0d82946abec12334b4810
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-05-15 16:53:59 -0300

    Adicionando elementos da pizza no HTML

--- compra-de-pizzas/script.js
+++ compra-de-pizzas/script.js
@@ -5,5 +5,10 @@ const selectElements = (e) => document.querySelectorAll(e);
 pizzaJson.map((item, index) => {
     let pizzaItem = selectElement('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
     selectElement('.pizza-area').append(pizzaItem);
 });
