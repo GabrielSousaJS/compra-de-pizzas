fc4f8976a0bdd211748dbb1aaf9ebdc1c6e55fbb
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-05-17 15:43:15 -0300

    Informações do modal preenchidas

--- compra-de-pizzas/script.js
+++ compra-de-pizzas/script.js
@@ -1,3 +1,5 @@
+let modalQuantity = 1;
+
 const selectElement = (e) => document.querySelector(e);
 const selectElements = (e) => document.querySelectorAll(e);
 
@@ -12,12 +14,27 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
-        
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQuantity = 1;
 
         selectElement('.pizzaBig img').src = pizzaJson[key].img;
         selectElement('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         selectElement('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        selectElement('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        
+        size = selectElement('.pizzaInfo--size.selected').classList.remove('selected');
+
+        selectElements('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            const indexBigPizza = 2;
+
+            if (sizeIndex === indexBigPizza) {
+                size.classList.add('selected');
+            }
+
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
+
+        selectElement('.pizzaInfo--qt').innerHTML = modalQuantity;
 
         selectElement('.pizzaWindowArea').style.opacity = 0;
         selectElement('.pizzaWindowArea').style.display = "flex";
